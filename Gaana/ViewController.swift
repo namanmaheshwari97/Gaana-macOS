8d2449fe00d131216153cb4fd9ab2b99a29c2b21
namanmaheshwari97 <namanmaheshwari97@gmail.com>
2016-11-19 10:38:55 +0530

    Initial Commit

---
+++ Gaana/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Gaana
+//
+//  Created by Naman Maheshwari on 19/11/16.
+//  Copyright © 2016 Naman Maheshwari. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
